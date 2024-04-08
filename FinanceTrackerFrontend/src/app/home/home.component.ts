b0a88002e38f2e7767c7ddfdbd23320c3b3dd11d
Kaankocc <kaankocc7515@gmail.com>
2024-04-07 19:59:36 -0700

    Modal working

--- FinanceTrackerFrontend/src/app/home/home.component.ts
+++ FinanceTrackerFrontend/src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  modalActive: boolean = false;
+
+  toggleModal() {
+    this.modalActive = !this.modalActive;
+  }
 }
